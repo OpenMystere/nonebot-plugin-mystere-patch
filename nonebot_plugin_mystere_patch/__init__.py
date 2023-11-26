9c0266380b80a596b57c9b13674b32b8e31196e5
Madray Haven <sgpublic2002@gmail.com>
2023-11-26 12:09:11 +0800

    init

---
+++ nonebot_plugin_mystere_patch/__init__.py
@@ -0,0 +1,72 @@
+from typing import Union, Any
+
+from nonebot.adapters.onebot.v11 import Bot as BotV11, Event as EventV11, Message as MessageV11, \
+    MessageSegment as MessageSegmentV11
+from nonebot.adapters.onebot.v12 import Bot as BotV12, Event as EventV12, Message as MessageV12, \
+    MessageSegment as MessageSegmentV12
+
+origin_v11_send = BotV11.send
+origin_v12_send = BotV12.send
+
+
+async def v11_send(
+        self: BotV11,
+        event: EventV11,
+        message: Union[str, MessageV11, MessageSegmentV11],
+        proactive: bool = False,
+        **params: Any,
+) -> Any:
+    event_dict = event.dict()
+    # 如果不是主动消息，则添加 origin_event 字段
+    if not proactive:
+        sub_type = ""
+        if "sub_type" in event_dict:
+            sub_type = event_dict["sub_type"]
+
+        origin_type = event_dict["type"]
+        if origin_type == "meta":
+            detail_type = event_dict["meta_event_type"]
+        elif origin_type == "message":
+            detail_type = event_dict["message_type"]
+        elif origin_type == "notice":
+            detail_type = event_dict["notice_type"]
+        elif origin_type == "request":
+            detail_type = event_dict["request_type"]
+        else:
+            raise ValueError("Unknown type of event: " + event_dict["type"])
+        params["origin_event"] = {
+            "id": event_dict["id"],
+            "type": event_dict["type"],
+            "detail_type": detail_type,
+            "sub_type": sub_type,
+        }
+    await origin_v11_send(self, event, message, **params)
+
+
+def v11_event_id(self: EventV11) -> str:
+    return self.dict()["id"]
+
+
+async def v12_send(
+        self: BotV12,
+        event: EventV12,
+        message: Union[str, MessageV12, MessageSegmentV12],
+        proactive: bool = False,
+        **params: Any,
+) -> Any:
+    event_dict = event.dict()
+    # 如果不是主动消息，则添加 origin_event 字段
+    if not proactive:
+        params["origin_event"] = {
+            "id": event_dict["id"],
+            "type": event_dict["type"],
+            "detail_type": event_dict["detail_type"],
+            "sub_type": event_dict["sub_type"],
+        }
+    await origin_v12_send(self, event, message, **params)
+
+
+BotV11.send = v11_send
+EventV11.get_id = v11_event_id
+
+BotV12.send = v12_send
